71a262e34660fdf172d4fc9f0bb8b860b6616a07
kalinovcic <lovro.kalinovcic@gmail.com>
2017-04-27 07:22:13 +0200

    Rewrite

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-aspelc src/main.aspel -o run_tree/kutikula -no-bytecode -old
+@echo off
+cl /nologo /O2 /DEBUG /Fo"run_tree/kutikula.obj" /Fe"run_tree/kutikula.exe" /MT /EHsc src/kutikula.cpp /link /nologo /subsystem:console kernel32.lib user32.lib gdi32.lib opengl32.lib advapi32.lib lib/SDL2.lib lib/glew32.lib
